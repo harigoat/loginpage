419e2404bf34d6c328e65f040239ef36d9705f4f
Hari Shankar <74664008+harigoat@users.noreply.github.com>
2025-07-11 16:40:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ def login():
     except FileNotFoundError:
         open("users.txt",'w').close()
     if user_found and user_correct:
-        return f"<h2>Welcome back, {username}!</h2>"
+        return render_template("defaultpage.html")
     elif user_found and not user_correct:
         return "<h3 style='color:red;'>Incorrect password. Try again.</h3>"
     else:
