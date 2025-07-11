f45a658e667205cdb254283a34107a5c3f9e5264
Hari Shankar <74664008+harigoat@users.noreply.github.com>
2025-07-11 13:00:36 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,49 @@
+from flask import Flask, redirect, render_template,request,url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form['username']
+    password = request.form['password']
+    
+    user_found = False
+    user_correct = False
+    
+    try:
+        with open("users.txt",'r') as f:
+            lines=f.readlines()
+            for line in lines:
+                stored_user , stored_pass = line.strip.split(":")
+                if stored_user == username:
+                    user_found = True
+                    if stored_pass == password:
+                        user_correct = True
+                        break
+    except FileNotFoundError:
+        open("users.txt",'w').close()
+    if user_found and user_correct:
+        return f"<h2>Welcome back, {username}!</h2>"
+    elif user_found and not user_correct:
+        return "<h3 style='color:red;'>Incorrect password. Try again.</h3>"
+    else:
+        return redirect(url_for("register", new_user=username))
+@app.route("/register", methods=["GET","POST"])
+def register():
+    if request.method=="POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        with open("users.txt", "a") as f:
+            f.write(f"{username}:{password}\n")
+    new_user = request.args.get("new_user", "")
+    return render_template("register.html", new_user=new_user)
+
+if __name__ == "__main__":
+    print("🚀 Flask app is running...")
+    app.run(debug=True)
+
+    
